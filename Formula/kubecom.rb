89412ce6d4de36c84a629625725fa3e64151cceb
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-05 22:43:36 +0300

    Brew formula update for kube-commander version 0.5.5-rc.2

--- Formula/kubecom.rb
+++ Formula/kubecom.rb
@@ -7,11 +7,11 @@ class Kubecom < Formula
 
   if OS.mac?
     url "https://github.com/<repo_owner>/<repo_name>/releases/download/0.5.5-rc.2/kubecom_0.5.5-rc.2_darwin_amd64.tar.gz", :using => CurlDownloadStrategy.
-    sha256 "b22782983d2b89676f8fd271abfa51d5fffc96ff7a58612ce4e35c52af37f09b"
+    sha256 "594c2e90c31c5eedcbc6b4d6141cde4ccc9044ed7cb20a9bf7ef260c6773c972"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/<repo_owner>/<repo_name>/releases/download/0.5.5-rc.2/kubecom_0.5.5-rc.2_linux_amd64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "00eb0a602b467d67ee323cecf2ca3e79d31acbf5c525bd72d28e1505829bb605"
+      sha256 "565ad03f5f9e50e64719cc783bedafcf216adf63dd15e4fbfd87ed68eb4d0033"
     end
   end
   
